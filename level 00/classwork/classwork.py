b103673bb45ac14380978ae274a84b372fe477b9
ChikviladzeGiorgi1 <giorgichikviladze720@gmail.com>
2024-10-12 06:00:11 -0700

    first commmit

---
+++ level 00/classwork/classwork.py
@@ -0,0 +1,9 @@
+print("giorgi")
+print("chikviladze")
+print(15)
+name= "gio"
+fullname="giochikviladze"
+age=15
+print(name)
+print(" fullname")
+print(age)
---
+++ level 01/homework/homework.py
@@ -0,0 +1,7 @@
+name = "giorgi"
+lastname = "chikviladze"
+age = 15
+height = 175
+learning_programming = True
+
+print("my name is", name + ". " "my lastname is", lastname + ". " "my age is", str(age) + ". " "my height is", str(height) + ". " "learning programing in GOA",str(learning_programming) + ".")
