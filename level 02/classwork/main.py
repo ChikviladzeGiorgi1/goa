1a661686e9e48e92ecf2c0778ba76ce0e395fd21
ChikviladzeGiorgi1 <giorgichikviladze720@gmail.com>
2024-10-28 11:38:32 -0700

    second commit

---
+++ level 02/classwork/main.py
@@ -0,0 +1,8 @@
+print("giorgi")
+print("chikviladze")
+print(15)
+
+name= "giorgi"
+surname="chikviladze"
+age=15
+print("my name is", name,"my surname is", surname,"my age is",age)
---
+++ level 02/homework/main.py
@@ -0,0 +1,27 @@
+# ცვლადი არის მონაცემის შემნახავი სტრუქტურა.
+print("giorgi")
+print("chikviladze")
+print("luka")
+print("saba")
+print("toko")
+print("nika")
+print("sandro")
+print("gega")
+print("cotne")
+print("irakli")
+print("zaza")
+print("mamuka")
+print("tika")
+print(14)
+print(56)
+print(67)
+print("tbilisi")
+print("saqartvelo")
+print(10)
+print(18)
+name="giorgi"
+lastname="chikviladze"
+age=15
+place="tbilisi"
+country="georgia"
+print(name, lastname, age, place, country)
