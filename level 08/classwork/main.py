171bda0d9a065805beb88494793c2afbf5756e35
ChikviladzeGiorgi1 <giorgichikviladze720@gmail.com>
2024-11-13 13:24:08 -0800

    Third commit

---
+++ level 08/classwork/main.py
@@ -0,0 +1,11 @@
+name=input("enter your name":)
+surname=input("enter your surname":)
+age=input("enter your age":)
+num1=input("enter your first number":)
+num2=input("enter your second number"":)
+print(name)
+print(surname)
+print(age)
+print(num1)
+print(num2)
+!
---
+++ level 08/homework/main.py
@@ -0,0 +1,25 @@
+#input-არის შეტანა
+#output- გამოტანა
+
+
+
+name=input("enter your name:")
+lastname=input("enter your surnmae: ")
+print( name )
+print( lastname )
+
+
+num1=10
+num2=5
+num3=1
+num4=2
+num5=4
+print((num1+num2)-num3*num4/num5%num2)
+print((num1-num2)-num3/num4+num5%num2**num1)
+num1=20
+num2=10
+num3=5
+num4=4
+num5=1
+print((num5-num4)/num1-num2+num3*num1+num2%num1**num2)
+print((num1*num2)/num3+num3-num1%num2**num1)
