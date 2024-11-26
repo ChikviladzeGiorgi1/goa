4e0ff85f77172dfaa711793cd56cae5567a9d357
ChikviladzeGiorgi1 <giorgichikviladze720@gmail.com>
2024-11-26 07:42:16 -0800

    fourth commit

---
+++ level 12/classwork/main.py
@@ -0,0 +1,23 @@
+print(True or False)
+#True
+print(True or False or True)
+#TRUE
+print(True or False or False)
+#True
+print(False or False)
+#False
+ 
+print(False and True)
+#False
+print(True and True)
+#True
+print(False and False)
+#False
+print(False and True and False)
+#False
+
+
+
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+print(num1 > 30 and num2 < 40)
