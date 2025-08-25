39d66c1b3f299f69f50ea4a8fa8a57bf98ff0b3d
ChikviladzeGiorgi1 <giorgichikviladze720@gmail.com>
2025-08-25 04:55:48 -0700

    second commit

---
+++ level 35/homework/main.py
@@ -0,0 +1,3 @@
+def remove_smallest(numbers):
+    return []
+min_value = min(numbers)
---
+++ level 36/homework/main.py
@@ -0,0 +1,17 @@
+def litres(time):
+    return int(time * 0,5)
+
+
+def number_to_string(num):
+    return str(num)
+num = 123
+text = str(num)
+print(text)
+
+
+
+def find_smallest_int(arr):
+    return min(arr)
+print(find_smallest_int([34, 15, 88, 2]))
+print(find_smallest_int([34, -345, -1, 100,]))
+    
