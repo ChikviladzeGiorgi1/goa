d9364b9ee5d15c9201966763554645abdf51f7b4
ChikviladzeGiorgi1 <giorgichikviladze720@gmail.com>
2025-08-28 16:08:56 -0700

    second commit

---
+++ level 41/homework/main.py
@@ -0,0 +1,28 @@
+for num in range(1,11):
+    print(num)
+    print(num**2)
+
+
+
+fruits = ["apple", "banana", "cherry", "mango"]
+
+for fruit in fruits:
+    print(fruit.upper())
+
+
+for num in range(0, 21, 2):
+    print(num)
+
+
+
+
+numbers = [3, 7, 2, 9, 4, 1]
+max_number = numbers[0]
+for num in numbers:
+    if num > max_number:
+        max_number = num
+
+print("max_number")
+
+
+
---
+++ level 47/homework/main.py
@@ -0,0 +1,16 @@
+fruits = ("ვაშლი", "ბანანი", "ატამი", "მსხალი", "საზამთრო")
+print( fruits)
+print(fruits[0])
+print(fruits[-1])
+
+
+
+my_tuple =(1,2,3,4,5,6)
+print(my_tuple[2])
+print(len(my_tuple))
+
+
+
+tuple_1 =(2,4,6,8)
+tuple_2 =(1,3,5,7)
+all_tuple =tuple_1 + tuple_2
